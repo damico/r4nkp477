52b56006346be14e88ee1cfa25321d0bc053aceb
jd.comment <jd.comment@69b45fde-2254-0410-930c-5d1857318ad2>
2008-08-11 16:39:21 +0000

    Added more test values for this unit test

--- RankPatt/src/org/jdamico/tests/FindSimPatTest.java
+++ RankPatt/src/org/jdamico/tests/FindSimPatTest.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import junit.framework.TestCase;
 
-import org.jdamico.components.FindSimPat;
+import org.jdamico.components.FindRankPat;
 import org.jdamico.dataobjects.FormObj;
 import org.jdamico.dataobjects.util.FormUtil;
 
@@ -13,7 +13,7 @@ public class FindSimPatTest extends TestCase {
 
 	public void testFindSimPat() throws Exception{
 		
-		FindSimPat findSp = new FindSimPat();
+		FindRankPat findSp = new FindRankPat();
 		findSp.process("jose", plotSampleData());
 		/* 
 		 * findSimPat("Jose",formMap)
@@ -29,6 +29,9 @@ public class FindSimPatTest extends TestCase {
 		formMap = FormUtil.getInstance().add2Map(formMap, FormUtil.getInstance().createForm(new String[] {"Owner:jose","1:1","2:2","3:3","4:4","5:5"}));
 		formMap = FormUtil.getInstance().add2Map(formMap, FormUtil.getInstance().createForm(new String[] {"Owner:jorge","1:4","2:5","3:3","4:3","5:5"}));
 		formMap = FormUtil.getInstance().add2Map(formMap, FormUtil.getInstance().createForm(new String[] {"Owner:paulo","1:3","2:2","3:5","4:4","5:5"}));
+		formMap = FormUtil.getInstance().add2Map(formMap, FormUtil.getInstance().createForm(new String[] {"Owner:adalberto","1:4","2:4","3:5","4:4","5:5"}));
+		formMap = FormUtil.getInstance().add2Map(formMap, FormUtil.getInstance().createForm(new String[] {"Owner:dulce","1:2","2:2","3:3","4:4","5:5"}));
+
 		
 		return formMap;
 	}

c36bd1db5472c0e07702fd3adb83443627c3fca0
jd.comment <jd.comment@69b45fde-2254-0410-930c-5d1857318ad2>
2008-08-25 17:09:17 +0000

    more java doc added

--- trunk/RankPatt/src/org/jdamico/dataobjects/util/FormUtil.java
+++ trunk/RankPatt/src/org/jdamico/dataobjects/util/FormUtil.java
@@ -10,14 +10,37 @@ import org.jdamico.config.Constants;
 import org.jdamico.dataobjects.AxisObj;
 import org.jdamico.dataobjects.FormObj;
 import org.jdamico.exceptions.RankPattException;
+import org.jdamico.tests.FindRankPattTest;
+
+/**
+ * Set of utilities to handle {@link FormObj}
+ * @author (Jose Damico) Jose Ricardo de Oliveira Damico
+ */
 
 public class FormUtil {
+	
+	/**
+	 * Static instance of this class to be used as singleton
+	 */
 	private static final FormUtil INSTANCE = new FormUtil();
+	
+	/**
+	 * Global point of access to FormUtil
+	 * Method implementation of Singleton Object Creational Pattern
+	 * @return only one FormUtil instance based on INSTANCE
+	 */
 	public static FormUtil getInstance(){
 		return INSTANCE;
 	}
 	
-	public FormObj createForm(String[] formData) throws RankPattException{
+	/**
+	 * Utility to create a {@link FormObj} given a String[] Dictionary.
+	 * @see {@link FindRankPattTest#plotSampleData()}
+	 * @param formDictionary String[] Dictionary
+	 * @return populated {@link FormObj}
+	 * @throws RankPattException in case of malformed formDictionary
+	 */
+	public FormObj createForm(String[] formDictionary) throws RankPattException{
 		
 		
 		Map<Integer, Integer> rankMap = new HashMap<Integer, Integer>();
@@ -27,10 +50,10 @@ public class FormUtil {
 		 * Check for only one valid Owner
 		 */ 
 		int count = 0;
-		count = formData.length;
-		if(count == 0 || count == 1) throw new RankPattException(Constants.INVALIDLST);
+		count = formDictionary.length;
+		if(count < 2) throw new RankPattException(Constants.INVALIDLST);
 		for(int i=0; i<count; i++){
-			String tmpItem = formData[i].trim();
+			String tmpItem = formDictionary[i].trim();
 			if(tmpItem.length()>2 && tmpItem.contains(Constants.SEPARATOR_TAG)){
 				if(tmpItem.contains(Constants.OWNER_TAG) 
 						&& tmpItem.length() > Constants.OWNER_TAG.length() 
@@ -65,15 +88,29 @@ public class FormUtil {
 		
 	}
 	
+	/**
+	 * Utility for {@link FormObj} Map creation. Where FormObject is the value and String formOwner is the key 
+	 * @param formMap a base map used to add a new pair of key & value
+	 * @param form New {@link FormObj} to be added at Map (as value)
+	 * @return the same Map passed as parameter, but with more one key/value pair
+	 * @throws RankPattException in case of passed Map already contains a key
+	 */
 	public Map<String, FormObj> add2Map(Map<String, FormObj> formMap, FormObj form) throws RankPattException{
 		
 		if(formMap.containsKey(form.getFormOwner())) throw new RankPattException(Constants.CONTAINSKEYFORM);
-		
 		formMap.put(form.getFormOwner(), form);
 		
 		return formMap;
 	}
 	
+	/**
+	 * It calculates the number of ranks (items, options or questions) then it maps all possible combinations. 
+	 * Each combination is a pair, that will populate a {@link AxisObj}
+	 * @param rankMap is the Map that contains ranks (items, options or questions) ant it values. It is a map 
+	 * that comes from FormObj
+	 * @return
+	 * @throws RankPattException
+	 */
 	public Map<Integer, AxisObj> distribute(Map<Integer, Integer> rankMap) throws RankPattException{
 		
 		//System.err.println("rankMap.size() => "+rankMap.size());
@@ -125,7 +162,7 @@ public class FormUtil {
 		return key;
 	}
 	
-	public boolean isDoublePair(String key) throws RankPattException{
+	private boolean isDoublePair(String key) throws RankPattException{
 		if(key.length()>2) throw new RankPattException(Constants.KEYTOOLONG);
 		boolean boolRet = false;
 		if(key.substring(1).equals(key.substring(0,1))) boolRet = true;
